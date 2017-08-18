4bbfffe7588aa11a2b161d569200594fa6549f58
MoniRoz <monika.rozwadowska@outlook.com>
2017-08-18 15:08:22 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devtool: 'eval-source-map',
+  entry: [
+    'babel-polyfill',
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    'react-hot-loader/patch',
+    path.join(__dirname, 'app/index.js')
+  ],
+  output: {
+    path: path.join(__dirname, '/dist/'),
+    filename: '[name].js',
+    publicPath: '/'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({template: 'app/index.tpl.html', inject: 'body', filename: 'index.html'}),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({'process.env.NODE_ENV': JSON.stringify('development')})
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      }, {
+        test: /\.json?$/,
+        loader: 'json'
+      }, {
+        test: /\.scss$/,
+        loader: 'style!css?modules&localIdentName=[name]---[local]---[hash:base64:5]!sass'
+      }, {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      }, {
+        test: /\.png$/,
+        loader: "url-loader?limit=100000"
+      }, {
+        test: /\.jpg$/,
+        loader: "file-loader"
+      }, {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/font-woff'
+      }, {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/octet-stream'
+      }, {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file'
+      }, {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=image/svg+xml'
+      }
+    ]
+  }
+};
